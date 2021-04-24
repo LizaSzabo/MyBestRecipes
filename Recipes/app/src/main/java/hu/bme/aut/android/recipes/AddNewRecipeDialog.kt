6998274f8f5b01cbed2d224de6e606f58da4a53a
LizaSzabo <Liza.Szabo0715@yahoo.ro>
2021-04-24 12:38:17 +0200

    spinner

--- Recipes/app/src/main/java/hu/bme/aut/android/recipes/AddNewRecipeDialog.kt
+++ Recipes/app/src/main/java/hu/bme/aut/android/recipes/AddNewRecipeDialog.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import hu.bme.aut.android.recipes.Model.Recipe
 import hu.bme.aut.android.recipes.databinding.DialogAddNewRecipeBinding
 import hu.bme.aut.android.recipes.databinding.DialogEditFragmentBinding
 
-class AddNewRecipeDialog : DialogFragment() {
+class AddNewRecipeDialog : DialogFragment(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: DialogAddNewRecipeBinding
     lateinit var addRecipeListener: AddRecipeListener
 
@@ -25,10 +28,55 @@ class AddNewRecipeDialog : DialogFragment() {
             addRecipeListener?.onNewRecipe(recipe)
             dismiss()
         }
+
+        val spinner: Spinner = binding.spinnerCategory
+        context?.let {
+            ArrayAdapter.createFromResource(
+                    it,
+                    R.array.categories,
+                    android.R.layout.simple_spinner_item
+            )
+                    .also { adapter ->
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        spinner.adapter = adapter
+                    }
+        }
+        spinner.onItemSelectedListener = this
+        spinner.dropDownHorizontalOffset = -20
         return binding.root
     }
 
     interface AddRecipeListener{
         fun onNewRecipe(recipe: Recipe)
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+         when(position){
+             0 -> setSelectedItem("Breakfast")
+             1 -> setSelectedItem("Lunch")
+             2 -> setSelectedItem("Beverage")
+             3 -> setSelectedItem("Appetizer")
+             4 -> setSelectedItem("Soup")
+             5 -> setSelectedItem("Main dish: Beef")
+             6 -> setSelectedItem("Main dish: Poultry")
+             7 -> setSelectedItem("Main dish: Pork")
+             8 -> setSelectedItem("Main dish: Seafood")
+             9 -> setSelectedItem("Main dish: Vegetarian")
+             10 -> setSelectedItem("Side dish: Vegetables")
+             11 -> setSelectedItem("Side dish: Other")
+             12 -> setSelectedItem("Dessert")
+             13 -> setSelectedItem("Canning / Freezing")
+             14 -> setSelectedItem("Bread")
+             15 -> setSelectedItem("Holidays")
+            else -> setSelectedItem("Entertaining")
+        }
+
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+   private fun  setSelectedItem(categorySelected: String){
+       binding.editTextRecipeCategory.setText(categorySelected)
+   }
 }
--- Recipes/app/src/main/java/hu/bme/aut/android/recipes/EditRecipeDialog.kt
+++ Recipes/app/src/main/java/hu/bme/aut/android/recipes/EditRecipeDialog.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import hu.bme.aut.android.recipes.Model.Recipe
 import hu.bme.aut.android.recipes.databinding.DialogEditFragmentBinding
 
-class EditRecipeDialog(val pos: Int, val actualRecipeData: Recipe): DialogFragment() {
+class EditRecipeDialog(val pos: Int, val actualRecipeData: Recipe): DialogFragment(), AdapterView.OnItemSelectedListener  {
     private lateinit var binding: DialogEditFragmentBinding
      lateinit var listener: EditRecipeListener
 
@@ -29,10 +32,56 @@ class EditRecipeDialog(val pos: Int, val actualRecipeData: Recipe): DialogFragme
             listener?.onRecipeEdited(recipe, pos)
             dismiss()
         }
+
+        val spinner: Spinner = binding.spinnerCategory
+        context?.let {
+            ArrayAdapter.createFromResource(
+                    it,
+                    R.array.categories,
+                    android.R.layout.simple_spinner_item
+            )
+                    .also { adapter ->
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        spinner.adapter = adapter
+                    }
+        }
+        spinner.onItemSelectedListener = this
+        spinner.dropDownHorizontalOffset = -20
         return binding.root
     }
 
     interface EditRecipeListener{
         fun onRecipeEdited(recipe: Recipe, pos: Int)
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when(position) {
+            0 -> setSelectedItem("Breakfast")
+            1 -> setSelectedItem("Lunch")
+            2 -> setSelectedItem("Beverage")
+            3 -> setSelectedItem("Appetizer")
+            4 -> setSelectedItem("Soup")
+            5 -> setSelectedItem("Main dish: Beef")
+            6 -> setSelectedItem("Main dish: Poultry")
+            7 -> setSelectedItem("Main dish: Pork")
+            8 -> setSelectedItem("Main dish: Seafood")
+            9 -> setSelectedItem("Main dish: Vegetarian")
+            10 -> setSelectedItem("Side dish: Vegetables")
+            11 -> setSelectedItem("Side dish: Other")
+            12 -> setSelectedItem("Dessert")
+            13 -> setSelectedItem("Canning / Freezing")
+            14 -> setSelectedItem("Bread")
+            15 -> setSelectedItem("Holidays")
+            16 -> setSelectedItem("Entertaining")
+            else -> setSelectedItem(binding.editTextRecipeCategory.text.toString())
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+     private fun setSelectedItem(categorySelected: String){
+            binding.editTextRecipeCategory.setText(categorySelected)
+        }
 }
